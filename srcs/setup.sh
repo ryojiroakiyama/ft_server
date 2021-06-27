34866728598a355db0eb16ef752d4cabd4982506
秋山諒次郎 <ryojiroakiyama@akiyamaryoujirounoMacBook-Air.local>
2021-06-27 18:52:33 +0900

    add all

--- srcs/setup.sh
+++ srcs/setup.sh
@@ -1,3 +1,7 @@
+if test "$AUTOINDEX" = "off"
+then
+sed -ie 's/autoindex on/autoindex off/g' ./etc/nginx/sites-available/ft_server.conf
+fi
 service mysql start
 service nginx start
 service php7.3-fpm start
